e61b6a02cf66c79de60d62d34542f587f1eb22e7
Ludger Solbach <soulman@soulspace.org>
2013-07-26 03:47:15 +0200

    refactored module.clj

--- module.clj
+++ module.clj
@@ -1,16 +1,16 @@
 [
- :name "CljModelGenerator"
+ :module "CljModelGenerator"
  :project "org.soulspace.clj"
- :type "framework"
+ :type :framework
  :version "0.3.0"
  :description "Generator framework for model driven software development"
- :plugins ["global" "deps" "clojure" "package"]
- :dependencies [["org.clojure" "clojure" "1.5.1"]
-                ["org.soulspace.clj" "CljLibrary" "0.3.0"]
-                ["org.soulspace.clj" "CljJavaLibrary" "0.2.0"]
-                ["oro" "oro" "2.0.8"]
-                ["org.soulspace.template" "TemplateEngine" "1.0.0"]
-                ["org.soulspace.modelling" "UML14Repository" "0.3.0"]
-                ["org.soulspace.modelling" "ModelRepository2" "0.3.0"]
-                ["org.soulspace.modelling" "UML14ModelBuilder" "0.3.0"]]
+ :plugins ["global" "sdeps" "clojure" "package"]
+ :dependencies [[["org.clojure" "clojure" "1.5.1"]]
+                [["org.soulspace.clj" "CljLibrary" "0.3.0"]]
+                [["org.soulspace.clj" "CljJavaLibrary" "0.2.0"]]
+                [["oro" "oro" "2.0.8"]]
+                [["org.soulspace.template" "TemplateEngine" "1.0.0"]]
+                [["org.soulspace.modelling" "UML14Repository" "0.3.0"]]
+                [["org.soulspace.modelling" "ModelRepository2" "0.3.0"]]
+                [["org.soulspace.modelling" "UML14ModelBuilder" "0.3.0"]]]
  ]
