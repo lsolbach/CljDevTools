6dd0466a2fcc4413a588386056517d3c7f941f82
Ludger Solbach <soulman@soulspace.org>
2013-08-03 06:50:48 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -4,7 +4,7 @@
  :type :framework
  :version "0.4.0"
  :description "Generator framework for model driven software development"
- :plugins ["global" "sdeps" "depsdot" "clojure" "package"]
+ :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
  :dependencies [[["org.clojure" "clojure" "1.5.1"]]
                 [["org.soulspace.clj" "CljJavaLibrary" "0.2.0"]]
                 [["org.soulspace.template" "TemplateEngine" "1.0.1"]]
