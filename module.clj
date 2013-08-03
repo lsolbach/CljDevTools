cdcb08f603869d507c2a8bed0fa43d1aa84dd550
Ludger Solbach <soulman@soulspace.org>
2013-08-03 23:11:18 +0200

    initial import, extracted from Baumeister

---
+++ module.clj
@@ -0,0 +1,9 @@
+[
+ :module "CljVersionLibrary"
+ :project "org.soulspace.clj"
+ :type :library
+ :version "0.1.0"
+ :description "The CljVersionLibrary contains abstractions and functions for versioning."
+ :plugins ["global" "dependencies" "clojure" "package"] ; "clojuretest"
+ :dependencies [[["org.clojure" "clojure" "1.5.1"]]]
+ ]
