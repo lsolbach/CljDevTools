f92474997e26f414282b97dbf1a209e7b81aae36
Ludger Solbach <soulman@soulspace.org>
2013-08-04 00:04:24 +0200

    updated module files

--- module.clj
+++ module.clj
@@ -5,6 +5,7 @@
  :version "0.4.3"
  :description "The CljArtifactLibrary contains abstractions and functions for the handling of artifacts."
  :plugins ["global" "dependencies" "clojure" "package"] ; "clojuretest"
- :dependencies [[["org.clojure" "clojure" "1.5.1"]
-                 ["org.soulspace.clj" "CljVersionLibrary" "0.4.3"]]]
+ :dependencies [[["org.clojure" "clojure" "1.5.1"]]
+                [["org.soulspace.clj" "CljLibrary" "0.4.0"]]
+                [["org.soulspace.clj" "CljVersionLibrary" "0.4.3"]]]
  ]
