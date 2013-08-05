9a320b023703f71e54eea2d4180e1cb241f08ef2
Ludger Solbach <soulman@soulspace.org>
2013-08-05 02:34:02 +0200

    added copyright notice in source files, updated module.clj

--- src/org/soulspace/clj/modelgenerator/template.clj
+++ src/org/soulspace/clj/modelgenerator/template.clj
@@ -1,3 +1,12 @@
+;
+;   Copyright (c) Ludger Solbach. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file license.txt at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+;
 (ns org.soulspace.clj.modelgenerator.template
   (:use
     [org.soulspace.clj file file-search]
@@ -32,5 +41,6 @@
 (defn get-template-engine [ctx gen]
   (let [engine (create-template-engine)
         template-files (get-template-files ctx gen)]
+    ;(println "TEMPLATES" template-files)
     (.loadTemplates engine (into-array template-files))
     engine))
