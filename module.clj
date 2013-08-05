62be2d6e1daa8a142303a93a2f67a57052587108
Ludger Solbach <soulman@soulspace.org>
2013-08-05 02:49:57 +0200

    updated module files, added copyright notice in source files

--- module.clj
+++ module.clj
@@ -4,6 +4,6 @@
  :type :library
  :version "0.4.3"
  :description "The CljVersionLibrary contains abstractions and functions for versioning."
- :plugins ["global" "dependencies" "clojure" "package"] ; "clojuretest"
+ :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
  :dependencies [[["org.clojure" "clojure" "1.5.1"]]]
  ]
--- src/org/soulspace/clj/version/version.clj
+++ src/org/soulspace/clj/version/version.clj
@@ -1,3 +1,12 @@
+;
+;   Copyright (c) Ludger Solbach. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file license.txt at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+;
 (ns org.soulspace.clj.version.version
   (:use [clojure.string :only [split]]))
 
@@ -64,7 +73,6 @@
               (compare-revision (first c1) (first c2))
               (recur (rest c1) (rest c2)))
             (compare-revision (first c1) (first c2)))))))
-    
   (lesser-version? [v1 v2] (< (compare-version v1 v2) 0))
   (greater-version? [v1 v2] (> (compare-version v1 v2) 0))
   (same-version? [v1 v2] (= (compare-version v1 v2) 0))
@@ -79,7 +87,7 @@
 (defmulti new-version type)
 (defmethod new-version String [v] (create-version v))
 (defmethod new-version VersionImpl [v] v)
-(defmethod new-version nil [v] (create-version nil))
+(defmethod new-version nil [v] (VersionImpl. nil))
 
 (defrecord VersionRangeImpl [from to]
   VersionRange
