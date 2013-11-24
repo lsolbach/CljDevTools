c2f08356c3f7aeca6184e68392254d2f1abfaa90
Ludger Solbach <soulman@soulspace.org>
2013-11-24 12:45:50 +0100

    enhanced and refactored pom model

--- module.clj
+++ module.clj
@@ -7,7 +7,7 @@
  :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
  :dependencies [["org.clojure/clojure, 1.5.1"]
                 ["org.clojure/data.zip, 0.1.1"]
-                ["org.soulspace.clj/CljApplicationLibrary, 0.5.1"]
                 ["org.soulspace.clj/CljXmlLibrary, 0.3.0"]
+                ["org.soulspace.clj/CljApplicationLibrary, 0.5.1"]
                 ]
  ]
