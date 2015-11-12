4a929b63e926145bc5704a0f291638de28d7a4d1
Ludger Solbach <soulman@soulspace.org>
2015-11-12 11:15:11 +0100

    added unmarshalling test for classpath

--- CljEclipseLibrary/unittest/org/soulspace/clj/eclipse/xml/test/classpath_test.clj
+++ CljEclipseLibrary/unittest/org/soulspace/clj/eclipse/xml/test/classpath_test.clj
@@ -1,18 +1,34 @@
 (ns org.soulspace.clj.eclipse.xml.test.classpath-test
 ;  (:import [org.soulspace.clj.eclipse.xml.classpath-model Classpath Classpathentry Attributes Attribute])
   (:use [clojure.test]
-        [org.soulspace.clj.xml marshalling]
-        [org.soulspace.clj.eclipse.xml.classpath-model]
-        [org.soulspace.clj.eclipse.xml.classpath-dsl])
+        [org.soulspace.clj.xml marshalling zip]
+        [org.soulspace.clj.eclipse.xml classpath-dsl classpath-model])
   (:require [clojure.data.xml :as xml]))
 
 (deftest marshalling-test
   (is (= (str "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
               "<classpath>"
-              "<classpathentry kind=\"out\" path=\"bin\"><attributes><attribute name=\"a\" value=\"1\"></attribute></attributes></classpathentry>"
+              "<classpathentry kind=\"out\" path=\"bin\">"
+              "<attributes><attribute name=\"a\" value=\"1\"></attribute></attributes>"
+              "</classpathentry>"
               "<classpathentry kind=\"src\" path=\"src\"></classpathentry>"
               "</classpath>")
          (xml/emit-str
            (to-xml (->Classpath
                      [(->Classpathentry "out" "bin" (->Attributes [(->Attribute "a" "1")]))
                       (->Classpathentry "src" "src" nil)]))))))
+
+(deftest unmarshalling-test
+  (is (= (->Classpath
+           [(->Classpathentry "out" "bin" (->Attributes [(->Attribute "a" "1")]))
+            (->Classpathentry "src" "src" nil)]))
+      (from-xml
+        (map->Classpath {})
+        (xml-zipper
+          (str "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+               "<classpath>"
+               "<classpathentry kind=\"out\" path=\"bin\">"
+               "<attributes><attribute name=\"a\" value=\"1\"></attribute></attributes>"
+               "</classpathentry>"
+               "<classpathentry kind=\"src\" path=\"src\"></classpathentry>"
+               "</classpath>")))))
