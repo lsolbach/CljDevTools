e1888006bf178598bb16f4e1f2f78f2ec1711441
Ludger Solbach <Ludger.Solbach@msg-systems.com>
2017-10-18 19:27:44 +0200

    updated project files

--- CljArtifactLibrary/module.clj
+++ CljArtifactLibrary/module.clj
@@ -2,7 +2,7 @@
  :module "CljArtifactLibrary"
  :project "org.soulspace.clj"
  :type :library
- :version "0.4.4"
+ :version "0.4.5"
  :description "The CljArtifactLibrary contains abstractions and functions for the handling of artifacts."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
  :scm-url "https://github.com/lsolbach/CljDevTools"
@@ -10,6 +10,6 @@
            ["org.soulspace.baumeister/ClojureTestPlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
  :dependencies [["org.clojure/clojure, 1.8.0"]
-                ["org.soulspace.clj/CljVersionLibrary, 0.4.4"]
-                ["org.soulspace.clj/CljLibrary, 0.6.1"]]
+                ["org.soulspace.clj/CljVersionLibrary, 0.4.5"]
+                ["org.soulspace.clj/CljLibrary, 0.7.0"]]
  ]
--- CljArtifactLibrary/project.clj
+++ CljArtifactLibrary/project.clj
@@ -1,9 +1,9 @@
-(defproject org.soulspace.clj/CljArtifactLibrary "0.4.4"
+(defproject org.soulspace.clj/CljArtifactLibrary "0.4.5"
   :description "The CljArtifactLibrary contains abstractions and functions for the handling of artifacts"
   :url "https://github.com/lsolbach/CljDevTools"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.soulspace.clj/CljVersionLibrary "0.4.4"]
-                 [org.soulspace.clj/CljLibrary "0.6.1"]]
+                 [org.soulspace.clj/CljVersionLibrary "0.4.5"]
+                 [org.soulspace.clj/CljLibrary "0.7.0"]]
   :test-paths ["unittest"])
